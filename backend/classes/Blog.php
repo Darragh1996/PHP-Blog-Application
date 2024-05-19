3619739058e9356750a30e08270dffe47ebd8753
Darragh Ferry <darraghferry@hotmail.com>
2024-05-19 23:22:49 +0100

    created blog class with some of the required functionality

---
+++ backend/classes/Blog.php
@@ -0,0 +1,75 @@
+<?php
+require_once 'Database.php';
+
+class Blog extends Database
+{
+    public $id;
+    public $user_id;
+    public $date;
+    public $text;
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    // get all blogs
+    public function getAll()
+    {
+        try {
+            $query = "SELECT * FROM blogs";
+            $stmt = $this->conn->query($query);
+
+            if ($stmt->execute()) {
+                $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
+                return $rows; // Return all fetched rows
+            }
+            return false;
+        } catch (PDOException $e) {
+            echo 'Error: ' . $e->getMessage();
+        }
+    }
+
+    // get single blog by user id
+    public function getByID()
+    {
+        try {
+            $query = "SELECT * FROM blogs WHERE id = :id";
+            $stmt = $this->conn->prepare($query);
+
+            // prevent SQL injection -> input values treated as data
+            // and not executable SQL
+            $stmt->bindParam(":id", $this->id);
+
+            if ($stmt->execute()) {
+                $row = $stmt->fetch(PDO::FETCH_ASSOC);
+                return $row; // Return the fetched row
+            }
+            return false;
+        } catch (PDOException $e) {
+            echo 'Error: ' . $e->getMessage();
+        }
+    }
+
+    // add a new blog 
+    public function add()
+    {
+        try {
+            $query = "INSERT INTO blogs (user_id, text) VALUES (:user_id, :text)";
+            $stmt = $this->conn->prepare($query);
+
+            // prevent SQL injection -> input values treated as data
+            // and not executable SQL
+            $stmt->bindParam(":user_id", $this->user_id);
+            $stmt->bindParam(":text", $this->text);
+
+
+            if ($stmt->execute()) {
+                return true;
+            }
+            return false;
+        } catch (PDOException $e) {
+            echo 'Error: ' . $e->getMessage();
+        }
+    }
+}
