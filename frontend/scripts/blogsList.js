3ebe52ac597c2739993b266a3fafc75047049ee6
Darragh Ferry <darraghferry@hotmail.com>
2024-05-23 23:06:52 +0100

    created a file to hold some functions to avoid initalising them more than once

--- frontend/scripts/blogsList.js
+++ frontend/scripts/blogsList.js
@@ -15,26 +15,3 @@ $(document).ready(() => {
     },
   });
 });
-
-let createBlogPost = (blogData) => {
-  // create the html structure of a blog post
-  let blog = $("<div></div>");
-  blog.addClass("blogPost");
-  let blogHeading = $("<div></div>");
-  blogHeading.addClass("blogHeading");
-  let blogTitle = $("<h3></h3>").text(blogData.title);
-  let blogAuthor = $("<p></p>").text("Author: " + blogData.username);
-  blogHeading.append(blogTitle, blogAuthor);
-  let blogDate = $("<p></p>").text("Posted: " + blogData.date);
-  blogDate.addClass("blogDate");
-  blog.append(blogHeading, blogDate);
-
-  // attach on click handler
-  blog.on("click", () => {
-    // window.location.href = "./blogs.html/" + blogData.id;
-    history.pushState(null, null, "/frontend/blogs/" + blogData.id);
-    handleRouteChange();
-  });
-
-  $("#blogListContainer").append(blog);
-};
---
+++ frontend/scripts/global.js
@@ -0,0 +1,25 @@
+// this script file is to avoid functions being declared
+// more than once when going back and forth between pages
+
+const createBlogPost = (blogData) => {
+  // create the html structure of a blog post
+  let blog = $("<div></div>");
+  blog.addClass("blogPost");
+  let blogHeading = $("<div></div>");
+  blogHeading.addClass("blogHeading");
+  let blogTitle = $("<h3></h3>").text(blogData.title);
+  let blogAuthor = $("<p></p>").text("Author: " + blogData.username);
+  blogHeading.append(blogTitle, blogAuthor);
+  let blogDate = $("<p></p>").text("Posted: " + blogData.date);
+  blogDate.addClass("blogDate");
+  blog.append(blogHeading, blogDate);
+
+  // attach on click handler
+  blog.on("click", () => {
+    // window.location.href = "./blogs.html/" + blogData.id;
+    history.pushState(null, null, "/frontend/blogs/" + blogData.id);
+    handleRouteChange();
+  });
+
+  $("#blogListContainer").append(blog);
+};
