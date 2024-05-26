1a07f655c9277086aa5096843ad5c3c0e90ae985
Darragh Ferry <darraghferry@hotmail.com>
2024-05-26 22:16:15 +0100

    can now add comments through site ui

--- frontend/scripts/blogs.js
+++ frontend/scripts/blogs.js
@@ -2,6 +2,35 @@ $(document).ready(() => {
   let pathname = window.location.pathname.replace(/^\/|\/$/g, "");
   let segments = pathname.split("/");
 
+  $("#addCommentForm").submit((e) => {
+    e.preventDefault();
+
+    let formData = {
+      text: $("#commentContent").val(),
+    };
+
+    $.ajax({
+      url: `${basePathBackend}/comments/${segments[3]}`,
+      type: "POST",
+      contentType: "application/json",
+      data: JSON.stringify(formData),
+      xhrFields: {
+        withCredentials: true,
+      },
+      success: (res) => {
+        history.pushState(
+          null,
+          null,
+          `${basePathFrontend}/blogs/${segments[3]}`
+        );
+        handleRouteChange();
+      },
+      error: (xhr, status, err) => {
+        console.error("comment creation failed: ", status, err);
+      },
+    });
+  });
+
   $.ajax({
     // segments[3] corresponds to the blog id we're tyring to access
     url: `${basePathBackend}/blogs/${segments[3]}`,
